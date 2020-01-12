420b4d72c550be72edce27cd154d286f64eedbdc
Martin <cph-ms782@cphbusiness.dk>
2020-01-12 22:03:57 +0100

    init files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-exam3sem
+[![Build Status](https://travis-ci.org/Simonkruse2/Course-Assignment-2.svg?branch=master)](https://travis-ci.org/Simonkruse2/Course-Assignment-2)
---
+++ src/main/webapp/precache-manifest.843a8cb8c7f65756f6f1a96bef34864e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6f37c128a1d212bb4869b22fbd5120",
+    "url": "./index.html"
+  },
+  {
+    "revision": "034c58dd61123eb12973",
+    "url": "./static/css/main.3dae8a31.chunk.css"
+  },
+  {
+    "revision": "66df46a9384d22c5bd39",
+    "url": "./static/js/2.b93ea3af.chunk.js"
+  },
+  {
+    "revision": "034c58dd61123eb12973",
+    "url": "./static/js/main.4d3d91ec.chunk.js"
+  },
+  {
+    "revision": "4232ce183f4effcb41e5",
+    "url": "./static/js/runtime-main.34fd541f.js"
+  }
+]);
